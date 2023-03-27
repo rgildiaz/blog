98e1980ba4e233c86963945e3f3d2b052ad562d2
Rafi Gil Diaz <rfgildiaz@gmail.com>
2023-03-26 21:30:52 -0500

    gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
