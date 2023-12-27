72b9d8ae727c98f0aab09b89d65cdc0d2b9575f0
Rafi Gil Diaz <rfgildiaz@gmail.com>
2023-12-26 22:55:06 -0600

    Initial commit (Contentlayer test)

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-J433OP7D.mjs
@@ -0,0 +1,33 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.md`,
+  contentType: "markdown",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of the post",
+      required: true
+    },
+    date: {
+      type: "date",
+      description: "The date of the post",
+      required: true
+    }
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) => `/posts/${doc._raw.flattenedPath}`
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "src/posts",
+  documentTypes: [Post]
+});
+export {
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-J433OP7D.mjs.map
---
+++ src/posts/test.md
@@ -0,0 +1,11 @@
+---
+title: This is a title
+date: 2023-12-26
+---
+
+## Finally
+
+```js
+const testing=`${window.getElementById('head')}`
+This is a test
+```
