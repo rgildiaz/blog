97f761ca168215be554630f1d158a7cb4ac7ea64
Rafi Gil Diaz <rfgildiaz@gmail.com>
2024-01-02 17:34:21 -0600

    style: update Tailwind attributes on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { SiteFooter } from '../components/site-footer'
 
 export default function Home() {
   return (
-    <div className='flex flex-col md:flex-row-reverse w-screen justify-start md:justify-center my-8 h-full px-4 max-w-screen'>
+    <div className='flex flex-col md:flex-row-reverse w-screen justify-start md:justify-center my-8 h-full px-2 max-w-screen'>
       <div className='md:ml-2 flex flex-col items-center md:items-start md:max-w-xl h-full overflow-visible'>
-        <div className='mb-4 w-full top-3 sticky backdrop-blur-lg bg-slate-200/50 p-3 rounded-lg'>
-          <h1 className="text-start text-5xl md:text-6xl font-sans font-light float-left mb-2" >{siteConfig.name}</h1>
-          <nav></nav>
+        <div className='mb-4 w-full top-3 sticky backdrop-blur-lg bg-slate-200/50 py-3 px-5 rounded-lg flex flex-row justify-between'>
+          <h1 className="text-start text-5xl md:text-6xl font-sans font-light mb-2" >{siteConfig.name}</h1>
+          <div className='h-full self-center justify-self-end text-4xl md:text-5xl hover:text-slate-500 hover:cursor-pointer transition-colors'>☼</div>
         </div>
-        <div className='text-md px-3'>
+        <div className='text-md px-1 md:px-3'>
           <p>This is where the about section and stuff will go. Making this line super long so I can see how it will wrap/behave.</p>
           <p>This is where the about section and stuff will go. Making this line super long so I can see how it will wrap/behave.</p>
           <p>This is where the about section and stuff will go. Making this line super long so I can see how it will wrap/behave.</p>
@@ -34,7 +34,7 @@ export default function Home() {
         </div>
         <SiteFooter className='hidden md:block' />
       </div>
-      <nav className='sticky top-6 h-fit md:mr-2 flex flex-col items-start md:items-end md:min-w-36 md:max-w-52 mt-8 md:mt-28 px-3 md:px-0'>
+      <nav className='sticky top-6 h-fit md:mr-2 flex flex-col items-start md:items-end md:min-w-36 md:max-w-52 mt-8 md:mt-28 px-1 md:px-0'>
         <div className='w-full flex flex-col md:items-end'>
           <Link href="/blog" className='group w-full flex md:justify-end backdrop-blur-lg bg-slate-200/50 p-3 rounded-lg'>
             <h2 className="text-2xl group-hover:italic">Blog</h2>
