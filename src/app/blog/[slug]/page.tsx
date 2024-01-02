6ce85f5526a4a67da5a288a9c2b740bbd6939add
Rafi Gil Diaz <rfgildiaz@gmail.com>
2024-01-02 17:35:18 -0600

    feat(blog): create blog layout and [slug] page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { format, parseISO } from 'date-fns'
 import { allPosts } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
+import { Mdx } from '@/src/components/mdx-components'
 
 export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
 
@@ -9,19 +11,26 @@ export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   return { title: post.title }
 }
 
+const getPost = (slug: string) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === slug)
+  if (!post) notFound()
+  return post
+}
+
 const PostLayout = ({ params }: { params: { slug: string } }) => {
-  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
+  const post = getPost(params.slug)
 
   return (
-    <article className="mx-auto max-w-xl py-8">
-      <div className="mb-8 text-center">
-        <h1 className="text-4xl md:text-5xl font-light">{post.title}</h1>
+    <article className="w-full py-8">
+      <div className="mb-8 text-left">
+        <h1 className="text-5xl md:text-5xl font-light">{post.title}</h1>
         <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
           {format(parseISO(post.date), 'LLLL d, yyyy')}
         </time>
       </div>
-      <div className="[&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      <div className="[&>*]:mb-3 [&>*:last-child]:mb-0">
+        <Mdx code={post.body.code} />
+      </div>
     </article>
   )
 }
--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,12 +1,26 @@
-import { SiteFooter } from "@/src/components/site-footer"
+import { PostsList } from '@/src/components/posts-list'
+import { SiteHeader } from '@/src/components/site-header'
+import { SiteFooter } from '@/src/components/site-footer'
 
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen w-full flex justify-center">
-      <div className="max-w-5xl w-full h-full mx-2 mt-3">
-        {children}
-        <SiteFooter />
+    <div className='flex flex-col md:flex-row-reverse w-screen justify-start md:justify-center my-8 h-full px-2 max-w-screen'>
+      <div className='md:ml-2 flex flex-col items-center md:items-start md:max-w-xl md:w-full h-full overflow-visible'>
+        <SiteHeader />
+        <div className='text-md px-1 md:px-3'>
+          {children}
+        </div>
+        <SiteFooter className='hidden md:block' />
       </div>
+      <nav className='sticky top-6 h-fit md:mr-2 flex flex-col items-start md:items-end md:min-w-36 md:max-w-52 mt-8 md:mt-28 px-1 md:px-0'>
+        <div className='w-full flex flex-col md:items-end'>
+          <div className='w-full flex md:justify-end'>
+            <h2 className="text-xl font-mono">more articles</h2>
+          </div>
+          <PostsList />
+        </div>
+      </nav>
+      <SiteFooter className='md:hidden block my-3' />
     </div>
   )
 }
