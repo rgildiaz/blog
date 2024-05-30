dc4d95a9a8751d11b78c0a3d41e26934f1844057
Rafi Gil Diaz <rfgildiaz@gmail.com>
2024-05-30 13:44:08 -0500

    feat(projects): create landing page for projects

---
+++ pages/projects/index.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>Projects</h1>
+      <p>Here are some of the projects I've worked on:</p>
+    </Layout>
+  );
+}
