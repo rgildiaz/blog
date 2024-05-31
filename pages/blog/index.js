e68d237501b64253a7b1fed0a35841899cf99dab
Rafi Gil Diaz <rfgildiaz@gmail.com>
2024-05-30 23:52:14 -0500

    feat(blog): create landing page

---
+++ pages/blog/index.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout/layout";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <p>🚧 under construction! 🚧</p>
+    </Layout>
+  );
+}
