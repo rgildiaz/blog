27ecd7fbce6258a9717ed049777e5f6ec0762f45
Rafi Gil Diaz <rfgildiaz@gmail.com>
2024-05-31 14:03:18 -0500

    fix(resume): add and style resume disclaimer

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -4,7 +4,18 @@ export default function Resume() {
   return (
     <Layout>
       <h1>Resume</h1>
-      <p>🚧 under construction! 🚧</p>
+      {/* probably not the best idea to mix inline styles and css modules like I am, but eventually I want to migrate over to Tailwind. It'll be easier to do that with inline styles than modules.*/}
+      <p
+        style={{
+          height: "50vh",
+          padding: "17vh 2ch",
+          color: "rgb(0, 0, 0, 0.2)",
+          fontStyle: "italic",
+        }}
+      >
+        i'm not actually sure how much i want to add this to the site, so this
+        might stay empty for a long time
+      </p>
     </Layout>
   );
 }
