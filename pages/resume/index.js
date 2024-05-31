8f23acb0c608a690c95dd89d5323e4805fe78e2a
Rafi Gil Diaz <rfgildiaz@gmail.com>
2024-05-31 13:47:57 -0500

    feat: create resume landing page

---
+++ pages/resume/index.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout/layout";
+
+export default function Resume() {
+  return (
+    <Layout>
+      <h1>Resume</h1>
+      <p>🚧 under construction! 🚧</p>
+    </Layout>
+  );
+}
