dda1ecee733e99fb2d1adff9b75ef239cc02c695
Rafi Gil Diaz <rfgildiaz@gmail.com>
2024-09-13 17:57:45 -0500

    feat: implement ProjectSection

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,6 +1,8 @@
 import Layout from "../../components/layout/layout";
 import Head from "next/head";
 
+import projectStyles from "./projects.module.css"
+
 export default function Projects() {
   return (
     <Layout>
@@ -8,7 +10,20 @@ export default function Projects() {
         <title>Rafi's projects</title>
       </Head>
       <h1>Projects</h1>
-      <p>Here are some of the projects I've worked on:</p>
+			<ProjectSection title="Music for Web Browsers" skills={["js", "react"]} />
+			<ProjectSection title="Word Music" skills={["scd"]} />
+			<ProjectSection title="genz" skills={["python", "scd"]} />
     </Layout>
   );
+}
+
+const ProjectSection = ({title, skills}) => {
+	return (
+		<section className={projectStyles.projectSection}>
+			<header>
+				<h2>{title}</h2>
+			</header>
+		</section>
+	)
+
 }
